f3a3fdab523aac89a110582e2b67e4c6d6077e9c
nauris badaitis <badaitisgustas@gmail.com>
2023-09-28 19:53:25 +0300

    pridetas atsitiktinis pazymiu generavimas

--- pati_pirma.cpp
+++ pati_pirma.cpp
@@ -1,4 +1,4 @@
-#include  "my_lib1.h"
+#include "my_lib1.h"
 
 struct Studentas {
     string Vardas, Pavarde;
@@ -7,6 +7,15 @@ struct Studentas {
     double Galutinis;
     double mediana;
 };
+
+void spausdVidurki(Studentas &temp){
+    
+    printf("%-10s%-15s%-10.2f\n", temp.Vardas.c_str(), temp.Pavarde.c_str(), temp.Galutinis, "");
+}
+  
+void spausdMediana(Studentas &temp){
+    printf("%-10s%-15s%-10s%-10.2f\n", temp.Vardas.c_str(), temp.Pavarde.c_str(), "", temp.mediana);
+}
 
 void vidurkis(Studentas &temp){
 double vidurkis = accumulate(temp.Pazymiai.begin(), temp.Pazymiai.end(), 0.0) / temp.Pazymiai.size();
@@ -26,29 +35,34 @@ void mediana(Studentas &temp) {
     }
 }
 
-void spausdMediana(Studentas &temp){
-    printf("%-10s%-15s%-10s%-10.2f\n", temp.Vardas.c_str(), temp.Pavarde.c_str(), "", temp.mediana);
-}
 
-void spausdVidurki(Studentas &temp){
-    
-    printf("%-10s%-15s%-10.2f\n", temp.Vardas.c_str(), temp.Pavarde.c_str(), temp.Galutinis, "");
-}
-
-int main() {
-    int n;
-    Studentas temp;
-    vector <Studentas> studentai;
-    cout << "Kiek studentu ivesite?" <<endl;
+void pildymas(Studentas &temp, vector<Studentas> &studentai){
+    char generuoti = 'n';
+    int n = 0;
+    int x; 
+    cout <<" Kiek norite ivesti studentu?" << endl;
     cin >> n;
+    for (int k=0; k<n; k++){
+    cout <<"Iveskite varda ir pavarde"<<endl;
+    cin >> temp.Vardas >> temp.Pavarde;
+    cout << " Ar norite sugeneruoti nd pazymius? Jei taip - Y, jei ne - N" << endl;
+    cin >> generuoti;
+    if (generuoti == 'Y' || generuoti == 'y'){
+    cout << "Kiek pazymiu uz namu darbus norite sugeneruoti?" <<endl;
+    cin >> x;
+    for (int i = 0; i<x; i++){
+        int pazymys = rand() % 10 +1;
+        temp.Pazymiai.push_back(pazymys);
+    }
+    temp.Egzaminas = rand() % 10 +1;
+    }
+    else if (generuoti == 'N' || generuoti == 'n')
+    {
+    cout<<"Iveskite egzamino paz."; 
+    cin>>temp.Egzaminas; 
+    cout << " Iveskite studento pazymius, jei pazymiu nebera, iveskite raide: " << endl;
     float x;
-    for (int i=0; i<n; i++){
-        cout <<"Iveskite varda ir pavarde"<<endl;
-        cin >> temp.Vardas >> temp.Pavarde;
-        cout<<"Iveskite egzamino paz."; 
-        cin>>temp.Egzaminas; 
-        cout << " Iveskite studento pazymius, jei pazymiu nebera, iveskite raide: " << endl;
-        while (cin>>x){
+    while (cin>>x){
         if (x>10) {
             cout<< " Ivestas neteisingas pazymys, bandykite dar karta"<<endl;
         }
@@ -56,30 +70,42 @@ int main() {
             temp.Pazymiai.push_back(x);
         }
     }
+    }
+
     vidurkis(temp);
     mediana(temp);
     studentai.push_back(temp);
     cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-
     
+}
+
+int main() {
+    Studentas temp;
+    vector <Studentas> studentai;
+    char uzkl='n';
+    char generuoti = 'y';
     char pasirinkimas = 'm';
-    cout << "Ar norite medianos (M) ar vidurkio (V)?" <<endl;
-    cin >> pasirinkimas;
-    printf("%-10s%-15s%-10s%-10s%-10s\n", "Vardas", "Pavarde", "Vidurkis", "Mediana", "");
-    for (int i = 0; i < 50; i++) {
-    cout << "-";
+    pildymas(temp, studentai);
+    
+        temp.Pazymiai.clear();
+        cout << "Ar norite medianos (M) ar vidurkio (V)?" <<endl;
+        cin >> pasirinkimas;
+        printf("%-10s%-15s%-10s%-10s%-10s\n", "Vardas", "Pavarde", "Vidurkis", "Mediana", "");
+        for (int i = 0; i < 50; i++) {
+        cout << "-";
+        }
+        cout <<endl;
+        if (pasirinkimas == 'm' || pasirinkimas == 'M'){
+        for (auto &i : studentai){
+        spausdMediana(i);
+        }
+        }
+        else{
+        for (auto &i : studentai){
+        spausdVidurki(i);
+     }
     }
-    cout <<endl;
-    if (pasirinkimas == 'm' || pasirinkimas == 'M'){
-    for (auto &i : studentai){
-    spausdMediana(i);
     }
-    }
-    else{
-    for (auto &i : studentai){
-    spausdVidurki(i);
-    }
-}
-}
+    
