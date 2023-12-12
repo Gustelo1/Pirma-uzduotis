211892b09b51ea68d066ac9ea2382322e1c78300
Gustelo1 <140171498+Gustelo1@users.noreply.github.com>
2023-12-12 21:40:23 +0200

    Delete spausdinimas.cpp

--- spausdinimas.cpp
+++
@@ -1,9 +0,0 @@
-#include "my_lib.h"
-void spausdVidurki(Studentas &temp){
-    
-    printf("%-10s%-15s%-10.2f\n", temp.Vardas.c_str(), temp.Pavarde.c_str(), temp.Galutinis, "");
-}
-  
-void spausdMediana(Studentas &temp){
-    printf("%-10s%-15s%-10s%-10.2f\n", temp.Vardas.c_str(), temp.Pavarde.c_str(), "", temp.mediana);
-}
